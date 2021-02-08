8d183c65e5582f96d131e0c83c83cb18b7eb2bb6
Benedict Lawrence <BTLawrence44@gmail.com>
2021-02-08 13:36:55 +0000

    refactoring rspec

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,25 +1,24 @@
 require 'bank_account' 
 
-describe Account do
+describe Account do 
+
+    let( :account ) { Account.new }
+
     it 'should return a bank balance of around 0' do 
-        account = Account.new
         expect(account.balance).to eq(0)
     end  
 
     it 'should return an empty array' do
-        account = Account.new 
         expect(account.transaction).to eq([["date", "credit", "debit", "balance"]])
     end
 
     describe 'deposite' do
         it 'should add money to the bank balance' do
-            account = Account.new 
             account.deposit(100) 
             expect(account.balance).to eq(100)
         end 
 
         it 'should return a deposite info from transaction' do
-            account = Account.new 
             account.deposit(100)
             expect(account.transaction).to eq([["date", "credit", "debit", "balance"], [Time.now.strftime("%d/%m/%Y"), 100, nil, 100]])
         end
@@ -27,28 +26,24 @@ describe Account do
 
     describe 'withdraw' do
         it 'should withdraw money from the account into the negitve' do
-            account = Account.new 
             account.withdraw(50) 
             expect(account.balance).to eq(-50)
         end 
 
         it 'should withdraw money from the account' do
-            account = Account.new 
             account.deposit(100) 
             expect(account.withdraw(50)).to eq(50)
         end 
 
         it 'should return withdraw information on the transation' do
-            account = Account.new  
             account.withdraw(100)
             expect(account.transaction).to eq([["date", "credit", "debit", "balance"], [Time.now.strftime("%d/%m/%Y"), nil, 100, -100]])
         end
     end 
 
     describe 'bank_statment' do 
-        it 'should return labels for date, credit, debit, balance' do
-            account = Account.new  
-            expect(account.bank_statment).to eq([["date", "credit", "debit", "balance"]]) 
+        it 'should return labels for date, credit, debit, balance' do 
+            expect(account.bank_statment).to eq([["date", "credit", "debit", "balance"]])
         end 
     end
 end
